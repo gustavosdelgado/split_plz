6e81b2c3893008b39f64f667a95c6f9a41f31bd6
gustavosdelgado <gustavodelgado@protonmail.com>
2022-11-02 19:04:25 -0300

    first commit

---
+++ splitplz/src/main/java/br/com/gustavosdelgado/splitplz/SplitplzApplication.java
@@ -0,0 +1,13 @@
+package br.com.gustavosdelgado.splitplz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitplzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitplzApplication.class, args);
+	}
+
+}
---
+++ splitplz/src/test/java/br/com/gustavosdelgado/splitplz/SplitplzApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gustavosdelgado.splitplz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitplzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
