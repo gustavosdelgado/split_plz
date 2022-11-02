1856fe1614a6250699ebe46356f1efc0c4f250f7
gustavosdelgado <gustavodelgado@protonmail.com>
2022-11-02 19:50:29 -0300

    adjusting folders and dependencies

---
+++ src/main/java/br/com/gustavosdelgado/splitplz/entity/User.java
@@ -0,0 +1,85 @@
+package br.com.gustavosdelgado.splitplz.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity(name = "app_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @OneToOne
+    private Role role;
+
+    private String email;
+
+    private String password;
+
+    private String salt;
+
+    private String name;
+
+    private String surname;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+}
---
+++ src/main/java/br/com/gustavosdelgado/splitplz/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package br.com.gustavosdelgado.splitplz.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import br.com.gustavosdelgado.splitplz.entity.Role;
+
+public interface RoleRepository extends CrudRepository<Role, Long> {
+
+}
